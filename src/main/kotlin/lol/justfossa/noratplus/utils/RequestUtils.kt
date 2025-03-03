401a88a1001cb0d66b64d36a40714eb7f23b2f7c
JustFossa <panko.marek@icloud.com>
2025-03-03 14:46:05 +0100

    prepared RequestUtils.kt

---
+++ src/main/kotlin/lol/justfossa/noratplus/utils/RequestUtils.kt
@@ -0,0 +1,16 @@
+package lol.justfossa.noratplus.utils
+
+import java.net.URLStreamHandler
+import java.net.URLStreamHandlerFactory
+
+
+class RequestUtils {
+
+    fun blockURL(): Void {
+        val streamHandler: URLStreamHandlerFactory? =  URLStreamHandlerFactory();
+
+
+    }
+
+
+}
